c10fc18715fdd0cf0a6341be997a18c25cb149e8
Mateo Balcorta <misterrattail@gmail.com>
2017-09-16 11:07:29 -0700

    cleaned up code.

--- app/commands/delete.js
+++ app/commands/delete.js
@@ -1,53 +1,29 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
+const {readAndParseData, writingToJsonFile} = require('../../taskStore.js')
 
-const readFileAndParse = (jsonPath, taskNumber) => {
-  fs.readFile(jsonPath, 'utf8', (err, data) => {
-    if(err){
-      throw err
-    }
-    runAfterRead(taskNumber, jsonPath, JSON.parse(data))
-    return JSON.parse(data);
-  });
+
+const ChangeIncompleteStatus = (jsonData, filePath, taskNumber) => {
+    const taskNumberIndex = (Number(taskNumber)-1);
+    const taskTitle = jsonData.tasks[taskNumberIndex].description;
+    const taskId = jsonData.tasks[taskNumberIndex].id
+    if(jsonData.tasks[taskNumberIndex] && jsonData.tasks[taskNumberIndex].incomplete){
+        jsonData.tasks[taskNumberIndex].incomplete = false;
+        writingToJsonFile(jsonData ,filePath)
+        process.stdout.write(`Deleted task ${taskId}: '${taskTitle}'\n`);
+      } else {
+        if(filePath === path.resolve(__dirname, '../tasks.json')){
+          process.stdout.write(`Task ${taskId}: "${taskTitle}" already deleted\n`);
+          }
+      }
 };
 
-const writeToFile = (filePath, objectTasksString) => {
-  fs.writeFile(filePath, objectTasksString, (err) => {
-    if(err) throw err
-    return objectTasksString;
-  });
-};
-
-const printToTerminal = (filePath, stringValue) => {
-  if(filePath === path.resolve(__dirname, '../tasks.json')){
-    process.stdout.write(stringValue);
-  }
-}
-
-const ChangeIncompleteStatus = (arrayOfObjects, taskNumber, filePath) => {
-  if(arrayOfObjects[taskNumber] && arrayOfObjects[taskNumber].incomplete){
-      const taskTitle = arrayOfObjects[taskNumber].description;
-      arrayOfObjects[taskNumber].incomplete = false;
-      return  `Deleted tasks ${taskNumber+1}: '${taskTitle}'\n`;
-    } else {
-      if(filePath === path.resolve(__dirname, '../tasks.json')){
-        process.stdout.write('ID Description\n');
-        process.stdout.write('-- -------------\n\n');
-        }
-      return 'You have 0 tasks\n';
-    }
-};
-
-const runAfterRead = (taskNumber, filePath, data) => {
-  const arrayOfTaskObjects = data.tasks;
-  const stringReturnValue = ChangeIncompleteStatus(arrayOfTaskObjects, (Number(taskNumber)-1), filePath);
-  const objectTasksString = JSON.stringify(data);
-  printToTerminal(filePath, stringReturnValue);
-  writeToFile(filePath, objectTasksString);
-  return stringReturnValue;
-}
 //complete will change incomplete status is false;
 exports.deleted = (taskNumber, filePath) => {
-  readFileAndParse(filePath, taskNumber);
+  if(taskNumber){
+    readAndParseData(filePath, ChangeIncompleteStatus, taskNumber);
+  } else {
+    throw new Error('Must enter task number to delete\n')
+  }
 };
