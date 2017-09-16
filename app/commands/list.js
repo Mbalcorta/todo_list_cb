4b66463bf2d0af6f63b2ef010d92279554b23d9d
Mateo Balcorta <misterrattail@gmail.com>
2017-09-16 11:21:39 -0700

    refactored list.

--- app/commands/list.js
+++ app/commands/list.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const path = require('path');
 const {readAndParseData} = require('../../taskStore.js')
 
-const filter = (arrayOfTasks) => {
-  return arrayOfTasks.filter((listObject)=>{
+const incompleteObjects = (tasksObject) => {
+  return tasksObject.tasks.filter((listObject)=>{
     return listObject.incomplete;
   });
 };
 
-const writeToTerminal = (arrayOfTasksObjects, jsonPath) => {
-
+const writeToTerminal = (data, jsonPath) => {
+    const arrayOfTasksObjects = incompleteObjects(data);
     if(jsonPath === path.resolve(__dirname, '../tasks.json')){
       process.stdout.write('ID Description\n');
       process.stdout.write('-- -------------\n');
@@ -20,16 +20,10 @@ const writeToTerminal = (arrayOfTasksObjects, jsonPath) => {
       let taskVariable;
       arrayOfTasksObjects.length === 1 ? taskVariable = 'task' : taskVariable = 'tasks'
       process.stdout.write(`\nYou have ${arrayOfTasksObjects.length} ${taskVariable}\n`);
-  }
-  //  return stringValue;
+    }
 };
 
-const runAfterRead = (data, jsonPath) => {
-  const tasksList = writeToTerminal(filter(data.tasks), jsonPath);
-  return tasksList;
-}
-
 exports.list = (jsonPath) => {
-    readAndParseData(jsonPath, runAfterRead);
+    readAndParseData(jsonPath, writeToTerminal);
 };
 
