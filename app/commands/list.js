774cd95807c249e566b35a69a17de44f3d512a58
Mateo Balcorta <misterrattail@gmail.com>
2017-09-19 10:57:06 -0700

    added callback to test list function.

--- app/commands/list.js
+++ app/commands/list.js
@@ -3,27 +3,28 @@ const fs = require('fs');
 const path = require('path');
 const {readAndParseData} = require('../../taskStore.js')
 
-const incompleteObjects = (tasksObject) => {
-  return tasksObject.tasks.filter((listObject)=>{
-    return listObject.incomplete;
-  });
-};
-
-const writeToTerminal = (data, jsonPath) => {
-    const arrayOfTasksObjects = incompleteObjects(data);
-    if(jsonPath === path.resolve(__dirname, '../tasks.json')){
-      process.stdout.write('ID Description\n');
-      process.stdout.write('-- -------------\n');
-      arrayOfTasksObjects.forEach((eachObject) => {
-          process.stdout.write(`${eachObject.id} ${eachObject.description}\n`);
+exports.list = (jsonPath, callback) => {
+  if(jsonPath){
+    readAndParseData(jsonPath, (err, data) => {
+      const arrayOfTasksObjects = data.tasks.filter((data) => {
+        return data.incomplete;
       });
+      let terminalString;
       let taskVariable;
       arrayOfTasksObjects.length === 1 ? taskVariable = 'task' : taskVariable = 'tasks'
-      process.stdout.write(`\nYou have ${arrayOfTasksObjects.length} ${taskVariable}\n`);
-    }
-};
-
-exports.list = (jsonPath) => {
-    readAndParseData(jsonPath, writeToTerminal);
+      if(jsonPath === path.resolve(__dirname, '../tasks.json')){
+        process.stdout.write('ID Description\n');
+        process.stdout.write('-- -------------\n');
+        arrayOfTasksObjects.forEach((eachObject) => {
+            process.stdout.write(`${eachObject.id} ${eachObject.description}\n`);
+        });
+        process.stdout.write(`\nYou have ${arrayOfTasksObjects.length} ${taskVariable}\n`);
+      }
+      terminalString = `\nYou have ${arrayOfTasksObjects.length} ${taskVariable}\n`
+      callback(err, terminalString);
+    });
+  } else {
+    throw new Error('Cannot find file\n')
+  }
 };
 
