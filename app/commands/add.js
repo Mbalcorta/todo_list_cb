0d3c9103c190eb77986e2f9d2a1f210f3512da5b
Mateo Balcorta <misterrattail@gmail.com>
2017-09-19 09:14:31 -0700

    function now has callback to work in test.

--- app/commands/add.js
+++ app/commands/add.js
@@ -3,33 +3,20 @@ const fs = require('fs');
 const path = require('path');
 const {readAndParseData, writingToJsonFile} = require('../../taskStore.js')
 
-
-const turnIntoObject = (taskString, taskNumber) => {
-  const taskObject = {id: taskNumber, description: taskString, incomplete: true};
-  return taskObject;
-};
-
-const pushNewObjectToTasksArray = (newTaskObject, allTasksObject) => {
-  allTasksObject.tasks.push(newTaskObject);
-  return allTasksObject;
-};
-
-const runAfterRead = (tasksObject, filePath, taskString) => {
-  let taskNumber =  tasksObject.tasks.length+1;
-  //turn string into object;
-  let newTaskObject = turnIntoObject(taskString, taskNumber);
-  //hold object and then push new array into tasks array
-  let modifiedTasksObject = pushNewObjectToTasksArray(newTaskObject, tasksObject);
-  //stringify tasks then write to jsonFile with new array added
-  writingToJsonFile(modifiedTasksObject, filePath);
-  if(filePath === path.resolve(__dirname, '../tasks.json')){
-    process.stdout.write(`Created task ${modifiedTasksObject.tasks.length}\n`);
-  }
-}
-
-exports.add = (taskString, filePath) => {
+exports.add = (taskString, filePath, callback) => {
   if(taskString){
-    readAndParseData(filePath, runAfterRead, taskString);
+    readAndParseData(filePath, function(err, data){
+      data.tasks.push({id: data.tasks.length+1, description: taskString, incomplete: true})
+      writingToJsonFile(filePath, data, function(err){
+          if(err){
+            throw err
+          }
+        if(filePath === path.resolve(__dirname, '../tasks.json')){
+          process.stdout.write(`Created task ${data.tasks.length}\n`);
+        }
+          callback()
+        })
+      })
   } else {
     //eventually make a test to catch an error when no string entered
     throw new Error('Error: must enter task\n') ;
